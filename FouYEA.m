d30827d41885c7153839d623c51127d086561df8
Adam D <reainasky@gmail.com>
2015-11-04 21:38:17 -0500

    Create FouYEA.m

---
+++ FouYEA.m
@@ -0,0 +1,11 @@
+function [X, w] = FourierSeries(x, T0, k_vec)
+syms t
+for mm = 1:length(k_vec)
+ k = k_vec(mm);
+ % Fourier series
+ X1(mm) = int(x*exp(-j*2*pi*k*t/T0), t, 0, T0)/T0
+ % change the symbolic value to numerical value
+ X(mm) = subs(X1(mm));
+ % angular frequency
+ w(mm) = k*2*pi/T0;
+end
